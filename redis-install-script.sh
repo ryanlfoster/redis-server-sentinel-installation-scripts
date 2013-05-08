2c49954714bf766100f729c6b90a9aae4a974d33
Niels Timmermans <nillisti@gmail.com>
2013-05-08 11:13:14 +0200

    Fix

--- redis-install-script.sh
+++ redis-install-script.sh
@@ -36,7 +36,7 @@ echo " 3: ... dir /var/lib/redis-server"
 echo " 4: ... loglevel notice"
 echo " 5: ... logfile /var/log/redis-server.log"
 echo "*****************************************"
-sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis-server\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis-server.log/" redis.conf > /etc/redis-server/redis.conf
+sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis-server\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis-server.log/" redis.conf
 sudo cp redis.conf /etc/redis-server
 echo "*****************************************"
 echo " 5. Configure Sentinel.Conf"
